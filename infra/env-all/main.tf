966e4a83a487f7f1387885686bd80179ac48794b
Vlad Holubiev <golubev.vld@gmail.com>
2017-09-24 14:44:17 +0300

    Renamed main state bucket

--- infra/env-all/main.tf
+++ infra/env-all/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region  = "eu-central-1"
   version = "~> 0.1.4"
+  profile = "vlad"
 }
 
-resource "aws_s3_bucket" "vlad_tf_state" {
-  bucket = "vlad-tf-state"
-  acl    = "private"
+resource "aws_s3_bucket" "vladholubiev_tf_state" {
+  bucket = "vladholubiev-tf-state"
 
   lifecycle {
     prevent_destroy = true
@@ -23,9 +23,10 @@ resource "aws_s3_bucket" "vlad_tf_state" {
 terraform {
   backend "s3" {
     acl     = "private"
-    bucket  = "vlad-tf-state"
+    bucket  = "vladholubiev-tf-state"
     key     = "env-all/main.tfstate"
     encrypt = "true"
     region  = "eu-central-1"
+    profile = "vlad"
   }
 }
