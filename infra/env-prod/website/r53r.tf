c719cfabf9eac710fb1af918f9119dbc612c4e35
Vlad Holubiev <golubev.vld@gmail.com>
2017-09-24 15:09:41 +0300

    infra: import main route53 NS record

--- infra/env-prod/website/r53r.tf
+++ infra/env-prod/website/r53r.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "vladholubiev_com" {
+resource "aws_route53_record" "vladholubiev_com_NS" {
   zone_id = "${aws_route53_zone.vladholubiev_com.zone_id}"
   name    = "${var.domain}"
   type    = "NS"
@@ -9,5 +9,16 @@ resource "aws_route53_record" "vladholubiev_com" {
     "ns-1869.awsdns-41.co.uk.",
     "ns-1039.awsdns-01.org.",
     "ns-362.awsdns-45.com.",
+  ]
+}
+
+resource "aws_route53_record" "vladholubiev_com_SOA" {
+  zone_id = "${aws_route53_zone.vladholubiev_com.zone_id}"
+  name    = "${var.domain}"
+  type    = "SOA"
+  ttl     = "900"
+
+  records = [
+    "ns-545.awsdns-04.net. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400",
   ]
 }
