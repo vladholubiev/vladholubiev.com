d7607f2d34a2d162d47afa74c51d9af8ea877cc5
Vlad Holubiev <golubev.vld@gmail.com>
2017-11-13 01:27:03 +0200

    infra: add source_code_hash to lambda to trigger updates

--- infra/env-prod/libreoffice/lambda.tf
+++ infra/env-prod/libreoffice/lambda.tf
@@ -1,12 +1,13 @@
 resource "aws_lambda_function" "convert" {
-  function_name = "${var.env}_${var.fn_name}"
-  handler       = "handler.handler"
-  runtime       = "nodejs6.10"
-  role          = "${aws_iam_role.convert.arn}"
-  s3_bucket     = "${aws_s3_bucket.serverless_libreoffice_pdf.id}"
-  s3_key        = "${aws_s3_bucket_object.package.key}"
-  memory_size   = 1536
-  timeout       = 25
+  function_name    = "${var.env}_${var.fn_name}"
+  handler          = "handler.handler"
+  runtime          = "nodejs6.10"
+  role             = "${aws_iam_role.convert.arn}"
+  s3_bucket        = "${aws_s3_bucket.serverless_libreoffice_pdf.id}"
+  s3_key           = "${aws_s3_bucket_object.package.key}"
+  memory_size      = 1536
+  timeout          = 25
+  source_code_hash = "${base64sha256(file(data.archive_file.convert.output_path))}"
 
   tracing_config {
     mode = "Active"
