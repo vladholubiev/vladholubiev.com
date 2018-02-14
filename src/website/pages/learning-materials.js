08b2efac305bfe0283971996c8eb4b097b11a839
Vlad Holubiev <golubev.vld@gmail.com>
2018-02-15 00:41:16 +0200

    feat: put colors to the rest of the links

--- src/website/pages/learning-materials.js
+++ src/website/pages/learning-materials.js
@@ -30,77 +30,90 @@ const data = [
     link: 'https://serverless.email/',
     description:
       'A weekly newsletter about serverless architectures and paradigms, function-as-a-service, AWS Lambda, etc.',
-    icon: 'ƛ'
+    icon: 'ƛ',
+    bgColor: '#e36b38'
   },
   {
     title: 'Node Weekly',
     link: 'https://nodeweekly.com',
     description: 'Weekly e-mail round-up of Node.js news and articles.',
-    icon: 'NODE'
+    icon: 'NODE',
+    bgColor: '#55bb22'
   },
   {
     title: 'Anti Patterns Catalog',
     link: 'http://wiki.c2.com/?AntiPatternsCatalog',
     description: '139 classical Anti Patterns',
-    icon: 'X'
+    icon: 'X',
+    bgColor: '#042fee'
   },
   {
     title: '97 Things Every Programmer Should Know',
     link:
       'https://97-things-every-x-should-know.gitbooks.io/97-things-every-programmer-should-know/content/en/index.html',
     description: 'Pearls of wisdom for programmers collected from leading practitioners',
-    icon: '97'
+    icon: '97',
+    bgColor: '#4183c4'
   },
   {
     title: '97 Things Every Software Architect Should Know',
     link:
       'https://manohars.files.wordpress.com/2009/11/97-things-every-software-architect-should-know.pdf',
     description: 'Similar to above, but more meta-level',
-    icon: '97'
+    icon: '97',
+    bgColor: 'black',
+    fgColor: 'white'
   },
   {
     title: `The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses`,
     link: 'https://www.amazon.com/Lean-Startup-Entrepreneurs-Continuous-Innovation/dp/0307887898',
     description:
       'Validated learning, rapid scientific experimentation, shorten product development cycles, measure actual progress without resorting to vanity metrics, and learn what customers really want',
-    icon: 'TLS'
+    icon: 'LS',
+    bgColor: '#027ac4'
   },
   {
     title: `mongoDB Weekly`,
     link: 'https://mongodb.email',
     description: 'Weekly e-mail round-up of MongoDB news and articles.',
-    icon: 'MONGO'
+    icon: 'MG',
+    bgColor: '#449a45'
   },
   {
     title: `DB Weekly`,
     link: 'https://dbweekly.com',
     description:
       'A weekly round-up of database technology news and articles covering new developments, SQL, NoSQL, document databases, graph databases, and more',
-    icon: 'DB'
+    icon: 'DB',
+    bgColor: '#478dda'
   },
   {
     title: 'Serverless Blog',
     link: 'https://serverless.com/blog',
     description: 'The blog on serverless & event-driven compute',
-    icon: 'SLS'
+    icon: 'SLS',
+    bgColor: '#FD5750'
   },
   {
     title: 'WebOps Weekly',
     link: 'https://webopsweekly.com',
     description: `A weekly newsletter on Web operations, infrastructure, performance, the backend, and tooling, from the browser down to the metal`,
-    icon: 'SLS'
+    icon: 'SLS',
+    bgColor: '#0093b1'
   },
   {
     title: 'Last Week in AWS',
     link: 'https://lastweekinaws.com/',
     description: `A weekly roundup of news from Amazon's cloud ecosystem- sprinkled with snark.`,
-    icon: 'AWS'
+    icon: 'AWS',
+    bgColor: '#f2cd00'
   },
   {
     title: 'Hacker News',
     link: 'https://news.ycombinator.com/',
     description: `¯\\_(ツ)_/¯`,
-    icon: 'HN'
+    icon: 'HN',
+    bgColor: '#ff6600'
   }
 ];
 
