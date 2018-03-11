7771bab79d64881d8ec2fde34d1b9cd6c12b8ef0
Vlad Holubiev <golubev.vld@gmail.com>
2018-03-11 19:45:34 +0200

    tneu: proxy tneu news html pages

--- infra/env-dev/tneu/cf.tf
+++ infra/env-dev/tneu/cf.tf
@@ -36,6 +36,20 @@ resource "aws_cloudfront_distribution" "tneu_scores" {
     }
   }
 
+  origin {
+    domain_name = "${aws_route53_record.tneu_edu_ua.name}"
+    origin_id   = "News-${aws_route53_record.tneu_edu_ua.name}"
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_protocol_policy   = "http-only"
+      origin_keepalive_timeout = 5
+      origin_read_timeout      = 10
+      origin_ssl_protocols     = ["SSLv3"]
+    }
+  }
+
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "${var.subdomain}.${var.domain}"
@@ -219,6 +233,40 @@ resource "aws_cloudfront_distribution" "tneu_scores" {
     min_ttl                = 0
     default_ttl            = 0
     max_ttl                = 0
+    compress               = true
+  }
+
+  cache_behavior {
+    allowed_methods = [
+      "GET",
+      "HEAD",
+      "OPTIONS",
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD",
+    ]
+
+    target_origin_id = "News-${aws_route53_record.tneu_edu_ua.name}"
+    path_pattern     = "/news/*.html"
+
+    forwarded_values {
+      query_string = true
+
+      query_string_cache_keys = [
+        "none",
+      ]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = "${7 * 24 * 60 * 60}"
+    max_ttl                = "${7 * 24 * 60 * 60}"
     compress               = true
   }
 
