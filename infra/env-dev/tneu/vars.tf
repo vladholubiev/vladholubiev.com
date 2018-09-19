1a5994dc2d26c2a1cc9a7d49560dc6ad5d175a8c
Vlad Holubiev <golubev.vld@gmail.com>
2018-09-20 00:16:40 +0300

    Revert "infra: remove tneu proxyc"
    This reverts commit f13e25a

---
+++ infra/env-dev/tneu/vars.tf
@@ -0,0 +1,27 @@
+variable "domain" {
+  default = "vladholubiev.com"
+}
+
+variable "subdomain" {
+  default = "tneu-api"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "droplet_ip" {
+  default = "138.68.77.58"
+}
+
+variable "tneu_edu_ua_ip" {
+  default = "193.104.213.106"
+}
+
+variable "droplet_port" {
+  default = "9101"
+}
+
+variable "droplet_port_news" {
+  default = "9191"
+}
