3c9c8771c2ea001e97c115234904fd00d3128a4c
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-15 21:19:54 +0200

    feat: mention github bug bounty

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default () => (
       </p>
 
       <p>
-        Fun fact - I made $2,500 by reporting 2 serious bugs to GitHub via{' '}
+        Fun fact - I made $2,555 from GitHub by reporting them 2 bugs via{' '}
         <ExternalLink url={'https://hackerone.com/vladholubiev'} text={'HackerOne'} />.
       </p>
 
