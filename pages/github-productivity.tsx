a1ee7f8e6d27b0cd504d9367decaad7eb487b110
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-22 20:17:49 +0200

    feat: add gh productivity page

---
+++ pages/github-productivity.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Typography} from 'antd';
+import Layout from '../components/Layout';
+
+const {Text} = Typography;
+
+export default () => (
+  <Layout>
+    <h1>GitHub Productivity</h1>
+
+    <Text>TBD</Text>
+  </Layout>
+);
