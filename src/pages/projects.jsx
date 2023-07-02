ae6e0c45c4d85dd11ff5262b07638aa84872f8f2
Vlad Holubiev <golubev.vld@gmail.com>
2023-07-02 18:14:37 +0200

    feat: upd hover color for project link

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -68,7 +68,7 @@ export default function Projects() {
               </h2>
               <Card.Description>{project.description}</Card.Description>
               <p
-                className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-ua-blue-500 dark:group-hover:text-ua-blue-300 dark:text-zinc-200">
                 <LinkIcon className="h-6 w-6 flex-none"/>
                 <span className="ml-2">{project.link.label}</span>
               </p>
