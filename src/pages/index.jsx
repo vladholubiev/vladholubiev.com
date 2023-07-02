624a349b565954369a8678f2eba1d40b91246ebe
Vlad Holubiev <golubev.vld@gmail.com>
2023-07-02 18:33:58 +0200

    feat: add read more button

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -15,6 +15,7 @@ import { formatDate } from "@/lib/formatDate";
 import { getAllArticles } from "@/lib/getAllArticles";
 import { Resume } from "@/components/Resume";
 import { GITHUB, INSTAGRAM, LINKEDIN, MEDIUM, STACKOVERFLOW, TWITTER } from "@/lib/social-links";
+import { Button } from "@/components/Button";
 
 
 function Article({ article }) {
@@ -114,13 +115,15 @@ export default function Home({ articles }) {
           </div>
         </div>
       </Container>
-      {/*<Photos/>*/}
       <Container className="mt-24 md:mt-28">
         <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col gap-16">
             {articles.slice(0, 3).map((article) => (
               <Article key={article.slug} article={article}/>
             ))}
+            <Button href="/articles" variant="secondary" className="w-full lg:w-1/2">
+              Read more
+            </Button>
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
             <Resume/>
