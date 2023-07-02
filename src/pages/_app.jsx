2855c1386a21d5b99cb09907dec952df1dc7b099
Vlad Holubiev <golubev.vld@gmail.com>
2023-07-02 18:18:02 +0200

    feat: add vercel analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react'
+import { Analytics } from '@vercel/analytics/react';
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
@@ -33,6 +34,7 @@ export default function App({ Component, pageProps, router }) {
         </main>
         <Footer />
       </div>
+      <Analytics />
     </>
   )
 }
