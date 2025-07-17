a34bb520611cf2aa52f82c726eba62905d3eed8f
Vlad Holubiev <3817380+vladholubiev@users.noreply.github.com>
2025-07-17 19:36:48 +0200

    docs: update article view count in metadata and intro

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -42,8 +42,7 @@ function Article({ article }: ArticleProps) {
 
 /*
 Refresh Medium Stats:
-1. Open https://medium.com/me/stats
-2. Run this in console: `$$('.js-statsTableRow td:nth-child(2) span.sortableTable-value,span.sortableTable-extraNumber').map(el => Number(el.innerText)).reduce((value, memo) => memo + value, 0)`
+Open https://medium.com/me/stats
  */
 
 interface ArticlesIndexProps {
@@ -57,12 +56,12 @@ const ArticlesIndex: NextPage<ArticlesIndexProps> = ({ articles }) => {
         <title>Articles - Vlad Holubiev</title>
         <meta
           name="description"
-          content="I write mostly about interesting problems I solve to spread the knowledge and help others. My articles on Medium were viewed over 284,521 times so far."
+          content="I write mostly about interesting problems I solve to spread the knowledge and help others. My articles on Medium were viewed over 310,678 times so far."
         />
       </Head>
       <SimpleLayout
         title="Blog"
-        intro="I write mostly about interesting problems I solve to spread the knowledge and help others. My articles on Medium were viewed over 284,521 times so far."
+        intro="I write mostly about interesting problems I solve to spread the knowledge and help others. My articles on Medium were viewed over 310,678 times so far."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
