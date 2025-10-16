6753f40d6537fc5f4788c84a9e8dd6578dee27c2
Vlad Holubiev <3817380+vladholubiev@users.noreply.github.com>
2025-10-16 15:58:05 +0200

    chore: migrate eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+ 
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+})
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: ['eslint:recommended', 'next', 'prettier'],
+  }),
+]
+ 
+export default eslintConfig
