69031db237f12f2a5f8c3262aef34cca512bf5b6
Vlad Holubiev <3817380+vladholubiev@users.noreply.github.com>
2025-10-16 16:26:51 +0200

    refactor: add type

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,13 @@
 import * as tailwindPlugin from 'prettier-plugin-tailwindcss'
+
+
 import shelfConfig from '@shelf/prettier-config' with { type: 'json' } ;
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
 export default {
   ...shelfConfig,
   plugins: [tailwindPlugin],
-}
+} ;
