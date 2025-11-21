49a46543610352f536faea74040064b2264e0379
Vlad Holubiev <3817380+vladholubiev@users.noreply.github.com>
2025-11-22 00:20:36 +0100

    feat: upgrade to next.js 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,25 @@
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
- 
+import next from 'eslint-config-next';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
-})
- 
+});
+
 const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  ...next,
   ...compat.config({
-    extends: ['next/core-web-vitals','next/typescript','eslint:recommended', 'next', 'prettier','plugin:prettier/recommended'],
+    extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
   }),
-]
- 
-export default eslintConfig
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+];
+
+export default eslintConfig;
